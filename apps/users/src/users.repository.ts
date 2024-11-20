a1596769322eb380a9fafc6272d11fc0e470d12f
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2024-11-20 16:39:44 +0700

    users: create, get, update, updateRefreshToken

--- apps/users/src/users.repository.ts
+++ apps/users/src/users.repository.ts
@@ -1,8 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { AbstractRepository } from '@app/shared';
 import { User } from '@app/shared/schemas';
+import { UserDto } from '@app/shared/dtos';
 
 @Injectable()
 export class UsersRepository extends AbstractRepository<User> {
@@ -10,5 +11,24 @@ export class UsersRepository extends AbstractRepository<User> {
 
   constructor(@InjectModel(User.name) userModel: Model<User>) {
     super(userModel);
+  }
+
+  async findByEmail(email: string) {
+    const document = await this.findOne({ email });
+    return document;
+  }
+
+  async updateById(_id: Types.ObjectId, data: Partial<UserDto>) {
+    const document = await this.findOneAndUpdate({ _id }, { $set: data });
+    return document;
+  }
+
+  async updateRefreshToken(_id: Types.ObjectId, refreshToken: string | null) {
+    const document = await this.findOneAndUpdate(
+      { _id },
+      { $set: { refreshToken } },
+    );
+
+    return document;
   }
 }
